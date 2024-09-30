aa71488b7567af5fbd6d5245f452fe75b2918cf6
koles <yg@htfv.com>
2024-09-30 19:37:28 +0700

    созданы модели Comments, Posts, Products, Users

--- core/models.py
+++ core/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class Comments(models.Model):
+    "База комментариев пользователей"
+    name = models.CharField(max_length = 50, verbose_name="Имя пользователя")
+    comment = models.TextField(max_length=200, verbose_name="Комментарий")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
+
+class Posts(models.Model):
+    "База постов для наполнения сайта"
+    published_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    title = models.CharField(max_length = 150, verbose_name="Заголовок")
+    context = models.TextField(verbose_name="Контекст")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural="Записи"
+
+
+class Products(models.Model):
+    "База продуктов для продажи"
+    title = models.CharField(max_length=150, verbose_name="Продукт")
+    description = models.TextField(verbose_name="Описание")
+    photo = models.ImageField(verbose_name="Фото")
+    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Цена")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
+class Users(models.Model):
+    "База пользователей"
+    name = models.CharField(max_length=150, verbose_name="Имя пользователя")
+    password = models.CharField(verbose_name="Пароль")
+    telephone = models.CharField(max_length=20, verbose_name='Телефон')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+        
